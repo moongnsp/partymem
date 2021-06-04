8daea61072634e5960245f932bd0f1041e329bf1
moongnsp <moon1q2w3e4r@gmail.com>
2021-06-05 02:38:11 +0900

    JS) validate 추가

--- partydata.js
+++ partydata.js
@@ -62,3 +62,15 @@ function onDelete(td) {
         resetForm();
     }
 }
+function validate() {
+    isValid = true;
+    if (document.getElementById("fullName").value == "") {
+        isValid = false;
+        document.getElementById("fullNameValidationError").classList.remove("hide");
+    } else {
+        isValid = true;
+        if (!document.getElementById("fullNameValidationError").classList.contains("hide"))
+            document.getElementById("fullNameValidationError").classList.add("hide");
+    }
+    return isValid;
+}
