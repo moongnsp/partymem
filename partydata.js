75e633f8793b8917f86294538f20acf6a4238ddb
moongnsp <moon1q2w3e4r@gmail.com>
2021-06-04 23:43:04 +0900

    html파일에 헷갈릴수있는 주석추가/js 코드추가

---
+++ partydata.js
@@ -0,0 +1,37 @@
+var selectedRow = null
+
+function onFormSubmit() {
+    if (validate()) {
+        var formData = readFormData();
+        if (selectedRow == null)
+            insertNewRecord(formData);
+        else
+            updateRecord(formData);
+        resetForm();
+    }
+}
+
+function readFormData() {
+    var formData = {};
+    formData["fullName"] = document.getElementById("fullName").value;
+    formData["email"] = document.getElementById("email").value;
+    formData["sex"] = document.getElementById("sex").value;
+    formData["p_number"] = document.getElementById("p_number").value;
+    return formData;
+}
+
+function insertNewRecord(data) {
+    var table = document.getElementById("employeeList").getElementsByTagName('tbody')[0];
+    var newRow = table.insertRow(table.length);
+    cell1 = newRow.insertCell(0);
+    cell1.innerHTML = data.fullName;
+    cell2 = newRow.insertCell(1);
+    cell2.innerHTML = data.email;
+    cell3 = newRow.insertCell(2);
+    cell3.innerHTML = data.sex;
+    cell4 = newRow.insertCell(3);
+    cell4.innerHTML = data.p_number;
+    cell4 = newRow.insertCell(4);
+    cell4.innerHTML = `<a onClick="onEdit(this)">Edit</a>
+                       <a onClick="onDelete(this)">Delete</a>`;
+}
